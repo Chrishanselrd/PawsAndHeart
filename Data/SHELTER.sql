6d51751007a89d15ae4c7bd7a64047d55575e8ae
Chrishansel Raphael Dimiputra <90863865+Chrishanselrd@users.noreply.github.com>
2024-02-15 22:31:30 -0800

    Add files via upload

---
+++ Data/SHELTER.sql
@@ -0,0 +1,21 @@
+-- Insert to SHELTER --
+insert into SHELTER values (1, 'Purrfect Pals', 'Seattle');
+insert into SHELTER values (2, 'Seattle Humane', 'Shoreline');
+insert into SHELTER values (3, 'Seattle Animal Shelter', 'Bellevue');
+insert into SHELTER values (4, 'Seattle Humane', 'Tacoma');
+insert into SHELTER values (5, 'Seattle Humane', 'Redmond');
+insert into SHELTER values (6, 'Seattle Animal Shelter', 'Lynnwood');
+insert into SHELTER values (7, 'Purrfect Pals', 'Everett');
+insert into SHELTER values (8, 'Seattle Animal Shelter', 'Bothell');
+insert into SHELTER values (9, 'Old Dog Haven', 'Covington');
+insert into SHELTER values (10, 'Old Dog Haven', 'Kent');
+insert into SHELTER values (11, 'Seattle Animal Shelter', 'Olympia');
+insert into SHELTER values (12, 'Purrfect Pals', 'Auburn');
+insert into SHELTER values (13, 'Old Dog Haven', 'Federal Way');
+insert into SHELTER values (14, 'Seattle Animal Shelter', 'Arlington');
+insert into SHELTER values (15, 'Seattle Animal Shelter', 'Woodinville');
+insert into SHELTER values (16, 'Purrfect Pals', 'North Creek');
+insert into SHELTER values (17, 'Old Dog Haven', 'SeaTac');
+insert into SHELTER values (18, 'Seattle Animal Shelter', 'Ballard');
+insert into SHELTER values (19, 'Seattle Humane', 'Green Lake');
+insert into SHELTER values (20, 'Old Dog Haven', 'Tukwila');
